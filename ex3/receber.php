e3e41cc10c7614ff14f3c4b21d029ed6aedf8776
Utilizador <Utilizador@WIN-MP3JADQD58A.local>
2020-11-12 12:24:00 +0000

    Update formulario.html, receber.php, and 2 more files...

--- ex3/receber.php
+++ ex3/receber.php
@@ -1,12 +1,10 @@
 <?php 
 	$num1 = $_POST['txt_num1'];
 	$num2 = $_POST['txt_num2'];
+	$operacao = $_POST['operacao'];
 
-	$soma = $num1 + $num2;
-	$sub = $num1 - $num2;
-	$mult = $num1 * $num2;
-	$div = $num1 / $num2;
-	
+
+
 	function soma ($num1, $num2){
 		$soma = 0;
 		$soma = $num1 + $num2;
@@ -30,8 +28,17 @@
 		$div = $num1 - $num2;
 		return $div;
 	}
-	echo "A soma é " .soma($num1, $num2);
-	echo "A subtracao é " .subtracao($num1, $num2);
-	echo "A multiplicacao é  " .multiplicacao($num1, $num2);
-	echo "A divisao é " .divisao($num1, $num2);
+
+	if ($operacao == 'soma') {
+		echo "A soma é " .soma($num1, $num2);
+	}
+	elseif ($operacao == 'sub') {
+		echo "A subtracao é " .subtracao($num1, $num2);
+	}
+	elseif ($operacao == 'mult') {
+		echo "A multiplicacao é  " .multiplicacao($num1, $num2);
+	}
+	elseif($operacao == 'div'){
+		echo "A divisao é " .divisao($num1, $num2);
+	}
 ?>
